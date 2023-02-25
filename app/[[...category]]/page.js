d430c4dcbb484e0a4f1b9b6fe10f52f3d188fb1c
Anar Mamedov <anar26271@gmail.com>
2023-02-25 12:47:26 +0400

    api connection

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -3,22 +3,54 @@ import HomeContainer from "@/containers/home";
 
 import Movies from "@/mocks/movies.json";
 
-async function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const API_URL = "https://api.themoviedb.org/3";
+
+const getSingleCategory = async (genreId) => {
+  const res = await fetch(`${API_URL}/discover/movie?api_key=${process.env.API_KEY}&with_genres=$(genreId)`);
+
+  return res.json();
+};
+
+const getCategories = async () => {
+  const res = await fetch(`${API_URL}/genre/movie/list?api_key=${process.env.API_KEY}&page=1`);
+
+  return res.json();
+};
+
+const getPopularMovies = async () => {
+  const res = await fetch(`${API_URL}/movie/popular?api_key=${process.env.API_KEY}&page=1`);
+
+  return res.json();
+};
+
+const getTopRatedMovies = async () => {
+  const res = await fetch(`${API_URL}/movie/top_rated?api_key=${process.env.API_KEY}&page=1`);
+
+  return res.json();
+};
 
 async function HomePage({ params }) {
-  await delay(20000);
   let selectedCategory;
 
+  const topRatedPromise = getTopRatedMovies();
+  const popularPromise = getPopularMovies();
+  const categoriesPromise = getCategories();
+
+  const [{ results: topRatedMovies }, { results: popularMovies }, { genres: categories }] = await Promise.all([topRatedPromise, popularPromise, categoriesPromise]);
+
   if (params.category?.length > 0) {
-    selectedCategory = true;
+    const { results } = await getSingleCategory(params.category[0]);
+    selectedCategory = results;
   }
+
   return (
     <HomeContainer
+      topRatedMovies={topRatedMovies}
+      popularMovies={popularMovies}
+      categories={categories}
       selectedCategory={{
         id: params.category?.[0] ?? "",
-        movies: selectedCategory ? Movies.results.slice(0, 7) : [],
+        movies: selectedCategory ? selectedCategory.slice(0, 7) : [],
       }}
     />
   );
